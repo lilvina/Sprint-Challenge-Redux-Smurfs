80c48fb1999f2c5410593e011219572a90149b52
lilvina <davinaleo2007@yahoo.com>
2019-03-08 10:29:54 -0800

    fixing some errors but page appears

---
+++ smurfs/src/components/SmurfList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+import { deleteSmurfs } from '../actions';
+
+const SmurfList = props => {
+  return (
+    <div className="smurf-list" key={props.id}>
+      <h2>{props.name}</h2>
+      <p>{props.age}</p>
+      <p>{props.height}</p>
+      <button onClick={props.deleteSmurfs} value={props.id}>Delete Smurf</button>
+    </div>
+  )
+
+}
+
+export default connect(null, {deleteSmurfs})(SmurfList)
